6c7872f4b0d42b120be57597010a002977750cf4
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-03-20 13:27:01 -0300

    A

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,34 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{
-		path: '',
-		loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage)
-	},
-	{
-		path: 'autoridade',
-		loadComponent: () => import('./pages/autoridade/autoridade.page').then((m) => m.AutoridadePage)
-	},
-	{
-		path: 'v/:id',
-		loadComponent: () => import('./pages/v-id/v-id.page').then((m) => m.VIdPage)
-	},
-	{
-		path: 'sobre/brapci',
-		redirectTo: 'about/brapci',
-		pathMatch: 'full'
-	},
-	{
-		path: 'about/:page',
-		loadComponent: () => import('./pages/sobre-brapci/sobre-brapci.page').then((m) => m.SobreBrapciPage)
-	},
-	{
-		path: 'doc',
-		loadComponent: () => import('./pages/doc/doc.page').then((m) => m.DocPage)
-	}
+  {
+    path: 'autoridade',
+    loadComponent: () => import('./pages/autoridade/autoridade.page').then((m) => m.AutoridadePage),
+  },
+  {
+    path: 'v/:id',
+    loadComponent: () => import('./pages/v-id/v-id.page').then((m) => m.VIdPage),
+  },
+  {
+    path: 'sobre/brapci',
+    redirectTo: 'about/brapci',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about/:page',
+    loadComponent: () =>
+      import('./pages/sobre-brapci/sobre-brapci.page').then((m) => m.SobreBrapciPage),
+  },
+  {
+    path: 'doc',
+    loadComponent: () => import('./pages/doc/doc.page').then((m) => m.DocPage),
+  },
+  {
+    path: 'revistas',
+    loadComponent: () => import('./pages/revistas/revistas.page').then((m) => m.RevistasPage),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  }
 ];
---
+++ src/app/pages/revistas/revistas.page.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { BrapciApiService } from '../../core/services/brapci-api.service';
+
+type Journal = {
+  id_jnl: string;
+  jnl_name: string;
+  jnl_name_abrev?: string;
+  jnl_issn?: string;
+  jnl_eissn?: string;
+  jnl_ano_inicio?: string;
+  jnl_ano_final?: string;
+  jnl_active?: string;
+  jnl_url?: string;
+  cover?: string;
+  jnl_collection?: string;
+  [key: string]: any;
+};
+
+@Component({
+  selector: 'app-revistas-page',
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './revistas.page.html',
+  styleUrl: './revistas.page.scss'
+})
+export class RevistasPage {
+  private readonly brapciApiService = inject(BrapciApiService);
+
+  readonly loading = signal(true);
+  readonly error = signal('');
+  readonly journals = signal<Journal[]>([]);
+
+  readonly hasResults = computed(() => this.journals().length > 0);
+
+  constructor() {
+    this.loadJournals();
+  }
+
+  loadJournals(): void {
+    this.loading.set(true);
+    this.error.set('');
+
+    this.brapciApiService.get<Journal[]>('brapci/source/journal').subscribe({
+      next: (response) => {
+        const items = Array.isArray(response) ? response : [];
+        this.journals.set(items);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.journals.set([]);
+        this.error.set('Não foi possível carregar as revistas. Tente novamente mais tarde.');
+        this.loading.set(false);
+      }
+    });
+  }
+}
