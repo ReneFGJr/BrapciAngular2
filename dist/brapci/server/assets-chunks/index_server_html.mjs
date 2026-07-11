a0dadc5489b37c9f4fdfdefbeb51544dd321e119
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-07-11 19:28:52 -0300

    A

--- dist/brapci/server/assets-chunks/index_server_html.mjs
+++ dist/brapci/server/assets-chunks/index_server_html.mjs
@@ -10,7 +10,7 @@ export default `<!doctype html>
   <meta property="og:description" content="Projeto Angular 20 da Brapci com recursos de SEO, autenticacao e multilingua.">
   <meta property="og:type" content="website">
   <link rel="icon" type="image/png" href="assets/img/favicon.png">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="bootstrap-icons/bootstrap-icons.min.css">
   <script src="env.js"></script>
 <link rel="stylesheet" href="/web2/styles-MADQMGTU.css"></head>
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
